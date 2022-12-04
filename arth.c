4e8b4fd3f55fb233bf0159cb48fe8a20b7705c83
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-04 11:46:23 +0530

    Add files via upload

---
+++ arth.c
@@ -0,0 +1,15 @@
+/* arthmetic */
+#include<stdio.h>
+ void main()
+{
+ int a=10;
+ int b=5;
+ int c=a+b;
+ int d=a-b;
+ int e=a*b;
+ int f=a/b;
+ printf("%d\n",c);
+ printf("%d\n",d);
+ printf("%d\n",e);
+ printf("%d\n",f);
+}
